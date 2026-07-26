13cbbe2c024377513de00863456977cf6ee23500
Lakshpreetkaur <lakshsadhioura03@gmail.com>
2026-07-26 16:13:08 +0530

    src

---
+++ src/exception.py
@@ -0,0 +1,6 @@
+''' Python already provides many toolboxes called MODULES.'''
+# sys - build-in python system module and all the functionalities present inside it we can use it so python interperter gets to know that we are using this thing from sys module 
+import sys
+
+def error_message_details(error,error_detail:sys):
+  
