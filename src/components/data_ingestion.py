db071dfc9348807700c50c6e5eb3f2ddab463da7
Lakshpreetkaur <lakshsadhioura03@gmail.com>
2026-07-31 03:02:35 +0530

    Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,69 @@
+#  read data from any source  - split data into train and test - dta transformation 
+
+import os
+import sys
+from src.exception import CustomException  # extracting or importing this func from another file whose source or path mentioned
+from src.logger import logging
+import pandas as pd  # as we need to work with dataframe
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+# class to save all inputs where to save test ,raw ,train data 
+#  All the inputs in data ingestion which is required we will give it to this class
+
+# using DECORATOR - dataclass  -> instead of using __init__ we can directly define our class
+@dataclass
+class DataIngestionConfig:
+  # defining class variable - >all the output will save in artifacts folder and we are giving the path 
+  #  giving these inputs to data ingestion component  so that data ingestion knows where to save trainpath , test path
+  train_data_path: str=os.path.join('artifacts' , "train.csv")  #train_csv - file name
+  test_data_path: str=os.path.join('artifacts' , "test.csv")
+  raw_data_path: str=os.path.join('artifacts' , "data.csv")
+
+
+#  for defining variables only - > dataclass
+#  if func are used in class - > use init
+class DataIngestion:
+    def __init__(self):
+      #  ingestion_config will consist of above three values train,test,raw because we need input to intialize
+      # when DataIngestion class called the above three path will be saved in this class
+      self.ingestion_config = DataIngestionConfig()
+
+    #  to read data from any source when it is saved we made below class
+    def initiate_data_ingestion(self):
+      logging.info("Entered the data ingestion method or component")
+      #  if any error come we can write in partiular manner using try except
+      # try and except blocks to handle errors (exceptions) gracefully so that your program does not crash when something goes wrong
+
+      #  1. READ THE DATA FROM ANY SOURCE _ CSV , API , anywhere
+      try:
+        df = pd.read_csv(os.path.join("notebook", "data", "stud.csv"))
+        logging.info('Read the dataset as dataframe')
+
+        os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+        #  here we r doing for csv but same manner we can do it for mongo db or any else also
+        df.to_csv(self.ingestion_config.raw_data_path,index = False,header=True)
+
+        logging.info("Train Test split initiated")
+        train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
+
+        train_set.to_csv(self.ingestion_config.train_data_path,index = False,header=True)
+
+        test_set.to_csv(self.ingestion_config.test_data_path,index = False,header=True)
+
+        logging.info("Ingestion of the data is completed")
+
+        return(
+          self.ingestion_config.train_data_path,
+          self.ingestion_config.test_data_path
+
+        )
+      except Exception as e:
+        raise CustomException(e,sys)
+
+if __name__ =="__main__":
+  obj=DataIngestion()
+  obj.initiate_data_ingestion()
+
--- src/exception.py
+++ src/exception.py
@@ -11,7 +11,7 @@ def error_message_detail(error,error_detail:sys):
   '''exc_tb ->this will give info about which error occured in which line , file and will be stored in this variable '''
 
   _,_,exc_tb = error_detail.exc_info()
-  file_name = exc_tb.tb_frame.f.code.co_filename
+  file_name = exc_tb.tb_frame.f_code.co_filename
   error_message = "Error occured in python script name[{0}] line number [{1}] error message[{2}]".format(
     file_name.exc_tb.tb_line.no,str(error)) # script name[{0}] line number [{1}] error message[{2}]
 
